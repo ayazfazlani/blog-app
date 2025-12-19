4e5822ca3b70ec1a026be4aae8302c50227a8496
Muhammad Ayaz <143396617+ayazfazlani@users.noreply.github.com>
2025-12-19 16:54:20 +0500

    Refactor Next.js configuration to an empty object and downgrade Next.js and eslint-config-next dependencies to version 15.1.3 in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Ensure build output is compatible with Vercel
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
